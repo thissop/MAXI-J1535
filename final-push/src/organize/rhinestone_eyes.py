7b039ccc13541dc2431a722f8b0240b23f100f98
thissop <68932815+thissop@users.noreply.github.com>
2022-07-08 08:16:37 -0700

    resume in afternoon
    need to fix k-fold and repeated k-fold

--- final-push/src/organize/rhinestone_eyes.py
+++ final-push/src/organize/rhinestone_eyes.py
@@ -1,17 +1,17 @@
 from qpoml import collection
-import matplotlib.pyplot as plt 
 
 spectrum_csv = '/mnt/c/Users/Research/Documents/GitHub/MAXI-J1535/final-push/data/sources/GRS_1915+105/spectral/spectral_summary_for_qpoml.txt'
 qpo_csv = '/mnt/c/Users/Research/Documents/GitHub/MAXI-J1535/final-push/data/sources/GRS_1915+105/qpo/qpo_summary_for_qpoml.txt'
 
 collection_one = collection()
-collection_one.load(qpo_csv=qpo_csv, context_csv=spectrum_csv, context_type='scalar',context_preprocess={'gamma':'normalize','kTe':'normalize','nthcomp_norm':'normalize','diskbb_tin':'normalize','diskbb_norm':'normalize'}, qpo_preprocess={'frequency':'normalize', 'width':'normalize', 'rms':'normalize'}, qpo_approach='single')
+collection_one.load(qpo_csv=qpo_csv, context_csv=spectrum_csv, context_type='scalar',context_preprocess={'gamma':'normalize','kTe':'normalize','nthcomp_norm':'normalize','diskbb_tin':'normalize','diskbb_norm':'normalize'}, qpo_preprocess={'frequency':'normalize', 'width':'normalize', 'rms':'normalize'})
 
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import RandomForestRegressor
 
 regr = RandomForestRegressor()
+collection_one.evaluate(model=regr, model_name='RandomForestRegressor', evaluation_approach='k-fold', folds=20)
 
-collection_one.evaluate(model=regr, model_name='RandomForestRegressor', evaluation_approach='k-fold', folds=20, repetitions=2)
+import matplotlib.pyplot as plt 
 
 fig, axs = plt.subplots(2,2, figsize=(12,12))
 
@@ -21,13 +21,64 @@ collection_one.plot_vif(ax=axs[1,0])
 collection_one.plot_feature_importances(kind='tree-shap', ax=axs[1,1])
 
 plt.tight_layout()
-plt.savefig('initial_results.png', dpi=150)
+plt.savefig('./final-push/src/organize/initial_results', dpi=150)
 
-fig, axs = plt.subplots(5,2, figsize=(3,15))
+collection_two = collection()
+collection_two.load(qpo_csv=qpo_csv, context_csv=spectrum_csv, context_type='scalar',context_preprocess={'gamma':'normalize','kTe':'normalize','nthcomp_norm':'normalize','diskbb_tin':'normalize','diskbb_norm':'normalize'}, qpo_preprocess={'frequency':'normalize', 'width':'normalize', 'rms':'normalize'})
+
+collection_three = collection() 
+collection_three.load(qpo_csv=qpo_csv, context_csv=spectrum_csv, context_type='scalar',context_preprocess={'gamma':'normalize','kTe':'normalize','nthcomp_norm':'normalize','diskbb_tin':'normalize','diskbb_norm':'normalize'}, qpo_preprocess={'frequency':'normalize', 'width':'normalize', 'rms':'normalize'})
+
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+
+names = []
+mses = []
+
+names.append('LinearRegression')
+mses.append(collection_one.get_performance_statistics()['mse'])
+
+'''
+regr = RandomForestRegressor(n_estimators=200)
+collection_two.evaluate(model=regr, model_name='RandomForestRegressor', evaluation_approach='k-fold', folds=20)
+
+names.append('RandomForest')
+mses.append(collection_two.get_performance_statistics()['mse'])
+
+regr = GradientBoostingRegressor(n_estimators=200)
+collection_three.evaluate(model=regr, model_name='RandomForestRegressor', evaluation_approach='k-fold', folds=20)
+
+names.append('GradientBoostingRegressor')
+mses.append(collection_three.get_performance_statistics()['mse'])
+'''
+
+import pandas as pd 
+import numpy as np
+
+df = pd.DataFrame(np.transpose(mses), columns=names)
+
+df.to_csv('/mnt/c/Users/Research/Documents/GitHub/MAXI-J1535/final-push/src/organize/mse_temp.csv',index=False)
+
+r'''
+
+print('first plot')
+fig, axs = plt.subplots(2,2, figsize=(12,12))
+
+collection_one.plot_correlation_matrix(ax=axs[0,0])
+collection_one.plot_dendrogram(ax=axs[0,1])
+collection_one.plot_vif(ax=axs[1,0])
+collection_one.plot_feature_importances(kind='tree-shap', ax=axs[1,1])
+
+plt.tight_layout()
+plt.savefig('./final-push/src/organize/initial_results', dpi=150)
+
+print('second plot')
+
+fig, axs = plt.subplots(5,2, figsize=(4,16))
 
 for i in range(0,5):
     collection_one.plot_results_regression(ax=axs[i,0], which=[0], feature_name='frequency', fold=i)
-    collection_one.plot_results_regression(ax=axs[i,1], which=[0], feature_name='frequency', fold=i)
+    collection_one.plot_results_regression(ax=axs[i,1], which=[0], feature_name='frequency', fold=i+5)
 
 #plt.tight_layout()
-plt.savefig('initial_results_regression.png', dpi=150)
+plt.savefig('./final-push/src/organize/rf_results_regression.png', dpi=150)
+'''
---
+++ final-push/src/organize/temp.py
@@ -0,0 +1,3 @@
+import seaborn as sns
+
+print(sns.color_palette())
