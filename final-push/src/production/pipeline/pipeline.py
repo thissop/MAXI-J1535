baf21cd7192fd127f44a1a4db382dd57c95b6310
thissop <68932815+thissop@users.noreply.github.com>
2022-10-04 21:54:29 -0700

    tonight tomorrow forever

--- final-push/src/production/pipeline/pipeline.py
+++ final-push/src/production/pipeline/pipeline.py
@@ -9,7 +9,13 @@ from sklearn.linear_model import LogisticRegression, LinearRegression
 from xgboost import XGBRegressor
 from sklearn.tree import DecisionTreeRegressor
 
-plt.style.use('/mnt/c/Users/Research/Documents/GitHub/QPOML/qpoml/stylish.mplstyle')
+wh1 = False
+
+if wh1: 
+    plt.style.use('/ar1/PROJ/fjuhsd/personal/thaddaeus/github/QPOML/qpoml/stylish.mplstyle')
+else: 
+    plt.style.use('/mnt/c/Users/Research/Documents/GitHub/QPOML/qpoml/stylish.mplstyle')
+
 sns.set_context("paper") #font_scale=
 sns.set_palette('deep')
 seaborn_colors = sns.color_palette('deep')
@@ -20,10 +26,10 @@ plt.rcParams["mathtext.fontset"] = "dejavuserif"
 #bi_cm = LinearSegmentedColormap.from_list("Custom", [seaborn_colors[0], (1,1,1), seaborn_colors[3]], N=20)
 
 def regression_pipeline(source:str, models:list, model_names:list,
-                        qpo_path:str, context_path:str, 
+                        qpo_path:str, context_path:str, repository_path:str,
                         qpo_preprocess_dict:dict, context_preprocess_dict:dict,
-                        model_hyperparameter_dictionaries:list,
-                        model_comparison_statistic:str='mae', k:int=10, repetitions:int=2, 
+                        model_hyperparameter_dictionaries:list,spectrum:bool=False,
+                        model_comparison_statistic:str='mae', k:int=10, repetitions:int=2, fold:int=0, wh1=wh1,
                         units:dict={'frequency':'Hz'}):
 
     r''' 
@@ -53,6 +59,12 @@ def regression_pipeline(source:str, models:list, model_names:list,
 
     alphabet = [str(chr(letter)).upper() for letter in range(97,123)] 
 
+    if repository_path[-1]!='/':
+        repository_path!='/'
+
+    qpo_path = f'{repository_path}{qpo_path}' 
+    context_path = f'{repository_path}{context_path}'
+
     # 2: Source-wise Machine Learning #
 
     n_total_qpos = len(pd.read_csv(qpo_path).index)
@@ -68,8 +80,10 @@ def regression_pipeline(source:str, models:list, model_names:list,
 
         model_name = model_names[model_index]
 
-        collec= collection()
-        collec.load(qpo_csv=qpo_path, context_csv=context_path, context_type='scalar',  
+        notation_string = f'[{model_name}][{source}][{spectrum}]'
+
+        collec = collection()
+        collec.load(qpo_csv=qpo_path, context_csv=context_path,
                     context_preprocess=context_preprocess_dict, qpo_preprocess=qpo_preprocess_dict, units=units, approach='regression') 
 
         # 2.1.1: GridSearch # 
@@ -84,10 +98,11 @@ def regression_pipeline(source:str, models:list, model_names:list,
         for temp_index in range(len(column_names)):
             best_configuration_df[column_names[temp_index]] = columns[temp_index]
 
-        best_configuration_df.to_csv(f'/mnt/c/Users/Research/Documents/GitHub/MAXI-J1535/final-push/output/pipeline/GRS_1915+105/{model_name}_BestParams.csv', index=False)
-
-        gridsearch_scores.append(scores)
-
+        if source.replace('_',' ') == 'GRS 1915+905': 
+            best_configuration_df.to_csv(f'{repository_path}final-push/output/pipeline/GRS_1915+105/{notation_string}_BestParams.csv', index=False)
+        else: 
+            best_configuration_df.to_csv(f'{repository_path}final-push/output/pipeline/MAXI_J1535-571/{notation_string}_BestParams.csv', index=False)
+        
         # 2.1.2: k-fold on Best Configuration # 
 
         collec.evaluate(model=model, evaluation_approach='k-fold', folds=k, repetitions=repetitions, hyperparameter_dictionary=best_params) # evaluate-approach???
@@ -98,7 +113,11 @@ def regression_pipeline(source:str, models:list, model_names:list,
 
         fold_performance_df = pd.DataFrame.from_dict(statistics)
         fold_performance_df['fold']=[i for i in range(len(fold_performance_df.index))]
-        temp_path = f'/mnt/c/Users/Research/Documents/GitHub/MAXI-J1535/final-push/output/pipeline/GRS_1915+105/{model_name}_k-fold.csv'
+        if source.replace('_',' ') == 'GRS 1915+905': 
+            temp_path = f'{repository_path}final-push/output/pipeline/GRS_1915+105/{notation_string}_k-fold.csv'
+        else: 
+            temp_path = f'{repository_path}final-push/output/pipeline/MAXI_J1535-571/{notation_string}_k-fold.csv'
+        
         fold_performance_df.to_csv(temp_path, index=False)
 
         fold_performances.append(list(fold_performance_df[model_comparison_statistic]))
@@ -106,17 +125,24 @@ def regression_pipeline(source:str, models:list, model_names:list,
         # 2.1.4: Plot Results Regression from 10th Fold # 
         fig, ax = plt.subplots(figsize=(4,4))
 
-        collec.plot_results_regression(feature_name='frequency', which=[0], ax = ax, fold=0)
-        temp_path = f'/mnt/c/Users/Research/Documents/GitHub/MAXI-J1535/manuscript/figures/figure_six/results-regression_fold={0}_model={model_name}'
+        collec.plot_results_regression(feature_name='frequency', which=[0], ax = ax, fold=fold)
+        temp_path = f'{repository_path}manuscript/figures/individual/figure_6/{notation_string}[results_regression][fold={fold}]'
         plt.savefig(f'{temp_path}.pdf')
         plt.savefig(f'{temp_path}.png', dpi=200)
         plt.close()
 
-        # 2.1.5: Plot Feature Importances from 10th Fold # 
+        # 2.1.5: Plot Feature Importances from fold-th Fold # 
         fig, ax = plt.subplots(figsize=(6,6))
-        imps_path = ''
-        collec.plot_feature_importances(model=model, fold=0, kind='tree-shap', style='box', ax=ax, save_path=imps_path)
-        temp_path = f'/mnt/c/Users/Research/Documents/GitHub/MAXI-J1535/manuscript/figures/figure_four/feature-importances_fold={0}_model={model_name}'
+        
+        collec.plot_feature_importances(model=model, fold=fold, kind='tree-shap', style='box', ax=ax, save_path=imps_path)
+        
+        temp_path = None 
+
+        if source.replace('_',' ') == 'GRS 1915+105':
+            temp_path = f'{repository_path}manuscript/figures/individual/figure_8/{notation_string}[feature_importances][fold={fold}]'
+        else: 
+            temp_path = f'{repository_path}manuscript/figures/individual/figure_9/{notation_string}[feature_importances][fold={fold}]'    
+        
         plt.savefig(f'{temp_path}.pdf')
         plt.savefig(f'{temp_path}.png', dpi=200)
         plt.close()
@@ -127,7 +153,7 @@ def regression_pipeline(source:str, models:list, model_names:list,
     fig, ax = plt.subplots()
     print(model_names, len(fold_performances))
     plot_model_comparison(model_names=model_names, performance_lists=fold_performances, style='violin', ax=ax, cut=0)
-    temp_path = f'/mnt/c/Users/Research/Documents/GitHub/MAXI-J1535/manuscript/figures/figure_four/{source}_model-comparison'
+    temp_path = f'{repository_path}manuscript/figures/individual/figure_4/[{notation_string}][model-comparison]'
     plt.savefig(f'{temp_path}.pdf') 
     plt.savefig(f'{temp_path}.png', dpi=200) 
     plt.close()
@@ -137,7 +163,7 @@ def regression_pipeline(source:str, models:list, model_names:list,
     fig, ax = plt.subplots()
     print(model_names, len(fold_performances))
     plot_model_comparison(model_names=model_names, performance_lists=fold_performances, style='violin', ax=ax)
-    temp_path = f'/mnt/c/Users/Research/Documents/GitHub/MAXI-J1535/manuscript/figures/figure_four/{source}_model-comparison__'
+    temp_path = f'{repository_path}manuscript/figures/individual/figure_4/{notation_string}[model_comparison:no-cut]'
     plt.savefig(f'{temp_path}.pdf') 
     plt.savefig(f'{temp_path}.png', dpi=200) 
     plt.close()
@@ -174,7 +200,7 @@ def regression_pipeline(source:str, models:list, model_names:list,
         for i in range(len(temp_columns)): 
             pairwise_results_df[temp_names[i]] = temp_columns[i]
         
-        temp_path = f'/mnt/c/Users/Research/Documents/GitHub/MAXI-J1535/manuscript/tables/{source}_pairwise-comparison'
+        temp_path = f'{repository_path}manuscript/tables/{notation_string}[pairwise-comparison]'
         
         pairwise_results_df.to_csv(f'{temp_path}.csv', index=False)
         pairwise_results_df.to_latex(f'{temp_path}.tex', index=False, float_format="%.2f")
@@ -183,11 +209,11 @@ def regression_pipeline(source:str, models:list, model_names:list,
 
         all_gridsearch_scores.append(gridsearch_scores)
 
-def classification_pipeline(source:str, models:list, model_names:list,
+def classification_pipeline(source:str, models:list, model_names:list,repository_path:str,
                             scalar_context_path:str, qpo_path:str, 
                             model_hyperparameter_dictionaries:list,
-                            context_preprocess_dictionary:dict, 
-                            k:int=10, repetitions:int=2): 
+                            context_preprocess_dictionary:dict, wh1=wh1, fold:int=0,
+                            k:int=10, repetitions:int=2, spectrum:bool=False): 
     
     import os
     import matplotlib.pyplot as plt
@@ -202,6 +228,12 @@ def classification_pipeline(source:str, models:list, model_names:list,
     
     all_gridsearch_scores = []
     source_observation_counts = []
+
+    if repository_path[-1]!='/':
+        repository_path+='/'
+    
+    qpo_path = f'{repository_path}{qpo_path}' 
+    context_path = f'{repository_path}{context_path}'
 
     # 2: Source-wise Machine Learning #
 
@@ -219,6 +251,8 @@ def classification_pipeline(source:str, models:list, model_names:list,
 
         model_name = model_names[model_index]
 
+        notation_string = f'[{model_name}][{source}][{spectrum}]'
+
         collec = collection()
         collec.load(qpo_csv=qpo_path, context_csv=scalar_context_path, context_preprocess=context_preprocess_dictionary, approach='classification', units={'frequency':'hz'})
         
@@ -232,18 +266,36 @@ def classification_pipeline(source:str, models:list, model_names:list,
         for temp_index in range(len(column_names)):
             best_configuration_df[column_names[temp_index]] = columns[temp_index]
 
-        best_configuration_df.to_csv(f'/mnt/c/Users/Research/Documents/GitHub/MAXI-J1535/final-push/output/pipeline/MAXI_J1535-571/{model_name}_BestParams.csv', index=False)
+        if source.replace('_',' ') == 'MAXI J1535-571':
+            best_configuration_df.to_csv(f'{repository_path}final-push/output/pipeline/MAXI_J1535-571/{notation_string}[BestParams].csv', index=False)
         gridsearch_scores.append(scores)
         
         collec.evaluate(model=model, evaluation_approach='k-fold', folds=k, stratify=True, repetitions=repetitions, hyperparameter_dictionary=best_params)
 
         # .1 Confusion Matrix # 
-        plt.style.use('/mnt/c/Users/Research/Documents/GitHub/QPOML/qpoml/stylish.mplstyle')
+
+
+        if wh1: 
+            plt.style.use('/ar1/PROJ/fjuhsd/personal/thaddaeus/github/QPOML/qpoml/stylish.mplstyle')
+        else: 
+            plt.style.use('/mnt/c/Users/Research/Documents/GitHub/QPOML/qpoml/stylish.mplstyle')
+
         sns.set_context('paper')
         fig, ax = plt.subplots(figsize=(4,4))
-        collec.plot_confusion_matrix(fold=0, ax=ax, labels=['No QPO', 'QPO'])
+        y_set = list(set(np.array(collec.y_test).flatten()))
+        cm_labels = None
+        cm_type = None
+        if len(y_set) == 2:
+            labels = ['No QPO', 'QPO']
+            cm_type = 'binary'
 
-        cm_path = f'/mnt/c/Users/Research/Documents/GitHub/MAXI-J1535/manuscript/figures/figure_seven/{model_name}-{source}-confusion_matrix'
+        else: 
+            labels = [f'{counter} QPO' for counter in y_set]
+            cm_type = 'multinomial'
+
+        collec.plot_confusion_matrix(fold=fold, ax=ax, labels=labels)
+
+        cm_path = f'{repository_path}manuscript/figures/individual/figure_7/{notation_string}[confusion_matrix:type={cm_type}]'
         fig.tight_layout()
         plt.savefig(f'{cm_path}.pdf')
         plt.savefig(f'{cm_path}.png', dpi=200)
@@ -252,33 +304,40 @@ def classification_pipeline(source:str, models:list, model_names:list,
         plt.close()
 
         # .2 ROC Curve # 
+        if cm_type == 'binary':
+            fig, ax = plt.subplots(figsize=(4,4))
+            if wh1: 
+                plt.style.use('/ar1/PROJ/fjuhsd/personal/thaddaeus/github/QPOML/qpoml/stylish.mplstyle')
+            else: 
+                plt.style.use('/mnt/c/Users/Research/Documents/GitHub/QPOML/qpoml/stylish.mplstyle')
+            sns.set_context('paper')
+            fpr, tpr, std_tpr, auc, std_auc = collec.roc_and_auc()
+            plot_roc(fpr=fpr, tpr=tpr, std_tpr=std_tpr, auc=auc, ax=ax, std_auc=std_auc)
 
-        fig, ax = plt.subplots(figsize=(4,4))
-        plt.style.use('/mnt/c/Users/Research/Documents/GitHub/QPOML/qpoml/stylish.mplstyle')
-        sns.set_context('paper')
-        fpr, tpr, std_tpr, auc, std_auc = collec.roc_and_auc()
-        plot_roc(fpr=fpr, tpr=tpr, std_tpr=std_tpr, auc=auc, ax=ax, std_auc=std_auc)
-
-        roc_path = f'/mnt/c/Users/Research/Documents/GitHub/MAXI-J1535/manuscript/figures/figure_eight/{model_name}-{source}-ROC'
-        fig.tight_layout()
-        plt.savefig(f'{roc_path}.pdf')
-        plt.savefig(f'{roc_path}.png', dpi=200)
+            roc_path = f'{repository_path}manuscript/figures/individual/figure_7/{notation_string}[ROC]'
+            fig.tight_layout()
+            plt.savefig(f'{roc_path}.pdf')
+            plt.savefig(f'{roc_path}.png', dpi=200)
 
         # .3 Feature Importances # 
 
         fig, ax = plt.subplots(figsize=(4,4))
-        plt.style.use('/mnt/c/Users/Research/Documents/GitHub/QPOML/qpoml/stylish.mplstyle')
+        if wh1: 
+            plt.style.use('/ar1/PROJ/fjuhsd/personal/thaddaeus/github/QPOML/qpoml/stylish.mplstyle')
+        else: 
+            plt.style.use('/mnt/c/Users/Research/Documents/GitHub/QPOML/qpoml/stylish.mplstyle')
+        
         sns.set_context('paper')
-        collec.plot_feature_importances(model, fold=0, style='box', ax=ax, kind='tree-shap')
-        fi_path = f'/mnt/c/Users/Research/Documents/GitHub/MAXI-J1535/manuscript/figures/figure_ten/{model_name}-{source}-Feature-Importances'
+        collec.plot_feature_importances(model, fold=fold, style='box', ax=ax, kind='tree-shap')
+        fi_path = f'{repository_path}manuscript/figures/individual/figure_9/{notation_string}[Feature-Importances]'
         fig.tight_layout()
         plt.savefig(f'{fi_path}.pdf')
         plt.savefig(f'{fi_path}.png', dpi=200)
 
         # save predictions, etc. 
-        statistics = collec.get_performance_statistics()
-
-        pd.DataFrame(statistics).to_csv(f'/mnt/c/Users/Research/Documents/GitHub/MAXI-J1535/final-push/output/pipeline/MAXI_J1535-571/{model_name}-performance_stats.csv', index=False)
+        statistics = pd.DataFrame(collec.get_performance_statistics())
+        if source.replace('_',' ') == 'MAXI J1535-571':
+            pd.DataFrame(statistics).to_csv(f'{repository_path}final-push/output/pipeline/MAXI_J1535-571/{notation_string}[Performance_stats].csv', index=False)
 
 model_hyperparameter_dictionaries = [{'normalize':[True, False]},
                                      {'min_samples_split':[4,6], 'min_samples_leaf':[3]}, 
@@ -286,20 +345,25 @@ model_hyperparameter_dictionaries = [{'normalize':[True, False]},
                                      {'min_samples_split':[4,6], 'min_samples_leaf':[1]}, 
                                      {'n_estimators':[500,1000], 'eta':[0.1]}]
 print('starting')
+
+# ROUND ONE: GRS REGRESSION # 
+
 regression_pipeline(source='GRS 1915+905', 
                     models=[LinearRegression(), DecisionTreeRegressor(), RandomForestRegressor(), ExtraTreesRegressor(), XGBRegressor()], 
                     model_names=['Linear', 'DT', 'RF', 'ET', 'XGBoost'], 
-                    qpo_path='/mnt/c/Users/Research/Documents/GitHub/MAXI-J1535/final-push/data/pipeline/regression/GRS_1915+105_QPO-Input.csv', 
-                    context_path='/mnt/c/Users/Research/Documents/GitHub/MAXI-J1535/final-push/data/pipeline/regression/GRS_1915+105_Scalar-Input.csv', 
+                    qpo_path='final-push/data/pipeline/regression/GRS_1915+105_QPO-Input.csv', 
+                    context_path='final-push/data/pipeline/regression/GRS_1915+105_Scalar-Input.csv', 
                     qpo_preprocess_dict={'frequency':'normalize', 'width':'normalize', 'rms':'normalize'}, 
                     context_preprocess_dict={'A':'normalize','B':'normalize','C':'normalize','D':'normalize','E':'normalize','F':'normalize','G':'normalize'}, 
                     model_hyperparameter_dictionaries=model_hyperparameter_dictionaries)
 
+# ROUND FOUR: MAXI CLASSIFICATION, SCALAR INPUT
+
 classification_pipeline(source='MAXI J1535-571', 
                         models=[RandomForestClassifier(), LogisticRegression()], 
                         model_names=['Random Forest', 'Logistic Regression'], 
-                        scalar_context_path='/mnt/c/Users/Research/Documents/GitHub/MAXI-J1535/final-push/data/pipeline/classification/MAXI_J1535-571_Scalar-Input.csv', 
-                        qpo_path='/mnt/c/Users/Research/Documents/GitHub/MAXI-J1535/final-push/data/pipeline/classification/MAXI_J1535-571_QPO-Input.csv',
+                        scalar_context_path='final-push/data/pipeline/classification/MAXI_J1535-571_Scalar-Input.csv', 
+                        qpo_path='final-push/data/pipeline/classification/MAXI_J1535-571_QPO-Input.csv',
                         model_hyperparameter_dictionaries=[{'n_estimators':[50,100,200]}, 
                                                            {'penalty':['l2'], 'C':[1, 5]}], 
                         context_preprocess_dictionary={'A':'normalize', 'B':'normalize', 'C':'normalize', 'D':'normalize', 'E':'normalize', 'F':'normalize', 'G':'normalize'}, 
